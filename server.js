0576eb3363119657395a84a4f28a69e7ac0f2dfd
aaron_chou <ferrari828@gmail.com>
2013-12-30 00:57:11 +0800

    Change app.js to server.js

--- server.js
+++ server.js
@@ -8,9 +8,22 @@ var express = require('express'),
     user = require('./routes/user'),
     http = require('http'),
       fs = require('fs'),
+  mongoose = require('mongoose'),
     path = require('path');
 
+mongoose.connect('mongodb://localhost/diary_database');
+
 var app = express();
+
+//Schemas
+var Diary = new mongoose.Schema({
+    author: String,
+    message: String,
+    releaseDate: Date,
+    picPath: String,    
+});
+
+var DiaryModel = mongoose.model('Diary', Diary);
 
 // all environments
 app.set('port', process.env.PORT || 4711);
@@ -31,7 +44,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/about', function(req, res){
+app.get('/about', function(req, res){ 
   res.render('about.jade', {title: 'About'});
 });
 app.get('/demo', function(req, res){
@@ -52,9 +65,26 @@ app.post('/images', function(req, res){
         fileName = req.body.fileName + '.png',
          tmpPath = req.body.path + '/' + fileName,
       targetPath = './public/uploads/' + fileName,
+         picDate = new Date(),
          message = '',
+           diary = new DiaryModel({
+              //author: req.body.author,
+              author: 'aaron',
+             message: '',
+             releaseDate: picDate,
+             picPath: '/uploads/' + fileName,
+          });
       binaryData = new Buffer(base64Data, 'base64').toString('binary');
-  console.log('--------------\n\n');
+  console.log('-------saved to database -------\n\n');
+  diary.save(function(err){
+    if(!err){
+      console.log('created!')
+    } else {
+      return console.log('err in created diary process!');
+    }
+    res.end();
+  });
+  
   //console.log(tmpPath);
   fs.writeFile(fileName, binaryData, 'binary', function(err, written, buffer){
     console.log(err);
@@ -72,11 +102,36 @@ app.post('/images', function(req, res){
     }  
   });
   
-  //console.log(binaryData);
-  
-  //console.log(req.file);
-  //res.send(data);
+  /*console.log(binaryData); 
+  console.log(req.file);
+  res.send(data);*/
   res.end();
+});
+
+app.put('/api/diaries/:id', function(req, res){
+  console.log('Update Diary: ' + req.params.id);
+  return DiaryModel.findById(req.params.id, function(err, pics){
+    pics.releaseDate = req.body.releaseDate;
+    //console.log(pics);  
+    return pics.save(function(err){
+      if(!err){
+        console.log('diary update!');
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
+app.get('/api/diaries', function(req, res){
+  return DiaryModel.find(function(err, diaries){
+    if(!err){
+      console.log(diaries);
+      return res.send(diaries);
+    } else {
+      return console.log(err);
+    }
+  });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
