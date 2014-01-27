a13b81a9cca9f9bb1bec12dca4828e542c3ccb49
aaron_chou <ferrari828@gmail.com>
2014-01-27 07:05:55 +0000

    Fixed Layout issue for Diary Detail Page

--- public/javascripts/views/photodataview.js
+++ public/javascripts/views/photodataview.js
@@ -3,6 +3,7 @@ var app = app || {},
 
 app.PhotoDataView = Backbone.View.extend({
    tagName: 'div',
+   className: 'visible-lg',
    events: {
      'click .btn-primary':'updateDiary',
      'click .btn-danger':'checkDiary',
@@ -12,30 +13,44 @@ app.PhotoDataView = Backbone.View.extend({
      //diary_id = this.model.id;
      this.listenTo(this.model, 'change', this.render);
      this.listenTo(this.model, 'destroy', this.remove);
-     $('#myModal').on('hidden.bs.modal', function(e){ 
-       console.log('----------- Test model event -----\n\n'); 
+     /*$('#myModal').on('hidden.bs.modal', function(e){ 
+       console.log('----------- Test model event 2014 -----\n\n'); 
        var delete_url = '/api/diaries/' + diary_id;
        $.ajax({
          url: delete_url,
          type: 'DELETE',
          success: function(data, textStatusm, jqXHR){
-           console.log('------ ajax delete complete -----\n\n');
+           console.log('------ ajax delete complete 2014/1/8 -----\n\n');
          },
          error: function(){
            console.log('------ ajax delete error -----\n\n');
          }
        });
-     });
+     });*/
      $('.btn-success').click(function(){
        console.log('----------- click success event -----\n\n');
+       var delete_url = '/api/diaries/' + diary_id;
+       $.ajax({
+         url: delete_url,
+         type: 'DELETE',
+         success: function(data, textStatusm, jqXHR){
+           console.log('------ ajax delete complete 2014/1/8 -----\n\n');
+         },
+         error: function(){
+           console.log('------ ajax delete error -----\n\n');
+         }
+       });
+       location.reload(); 
        $('#myModal').modal('hide');
      });
    },
    updateDiary: function(e){
      e.preventDefault();
-     //alert('updateDiary '); a
-     var textarea = $('#'+ this.model.id).val();
-     this.model.save({message: textarea});
+     //alert('updateDiary '); 
+     var textarea = $('#'+ this.model.id).val(),
+         titlearea = $('#t'+ this.model.id).val();
+     alert(titlearea);
+     this.model.save({message: textarea,title: titlearea});
    },
    deleteDiary: function(e){
      //alert('deleteDiary ' + this.model.id);
@@ -50,7 +65,7 @@ app.PhotoDataView = Backbone.View.extend({
    },
    //className: 'col-xs-6 col-md-5 col-sm-5 col-lg-4',
 
-   template: _.template('<div class="form-group"><label>Year:</label><input placeholder=<%= year %> disabled="disabled" class="form-control"></div><div class="form-group"><label>Month:</label><input placeholder=<%= month %> disabled="disabled" class="form-control"></div><div class="form-group"><label>Day:</label><input placeholder=<%= day %> disabled="disabled" class="form-control"></div><div class="form-group"><label> Note:</label><textarea rows="3" id=<%= _id  %> class="form-control"><%= message %></textarea></div><div class="caption"><button type="button" class="btn btn-primary aaron">Update</button>&nbsp<button type="button" class="btn btn-danger">Delete</button></div><br><br><br><br><br><br><br><br><br><br><br><br><br>'),
+   template: _.template('<div class="form-group"><label>Year:</label><input placeholder=<%= year %> disabled="disabled" class="form-control"></div><div class="form-group"><label>Month:</label><input placeholder=<%= month %> disabled="disabled" class="form-control"></div><div class="form-group"><label>Day:</label><input placeholder=<%= day %> disabled="disabled" class="form-control"></div><div class="form-group"><div class="form-group"><label>Title</label><textarea rows="1" id=t<%= _id %> class="form-control"><%= title %></textarea></div><label> Note:</label><textarea rows="3" id=<%= _id  %> class="form-control"><%= message %></textarea></div><div class="caption"><button type="button" class="btn btn-primary aaron">Update</button>&nbsp<button type="button" class="btn btn-danger">Delete</button></div><br>'),
 
    render: function(){
      //var html = '<h3>' + this.model.get('author') + '</h3>';
--- public/javascripts/views/photodetailview.js
+++ public/javascripts/views/photodetailview.js
@@ -2,10 +2,10 @@ var app = app || {};
 
 app.PhotoDetailView = Backbone.View.extend({
    tagName: 'div',
-   className: 'aaron',
+   className: 'visible-lg',
    //className: 'col-xs-6 col-md-7 col-sm-7 col-lg-8',
 
-   template: _.template('<img src=<%= picPath %> alt="Generic placeholder image" class="featurette-image img-responsive"><br><br><br>'),
+   template: _.template('<img src=<%= picPath %> alt="Generic placeholder image" class="featurette-image img-responsive"><br><br>'),
 
    render: function(){
      //var html = '<h3>' + this.model.get('author') + '</h3>';
