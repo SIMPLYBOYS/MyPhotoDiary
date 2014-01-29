6bc3699d0b8f5efe31f04326c247dc18059f4793
aaron_chou <ferrari828@gmail.com>
2014-01-29 03:14:04 +0000

    Add Time Stamp to each CardUI View

--- public/javascripts/views/photoview.js
+++ public/javascripts/views/photoview.js
@@ -4,7 +4,7 @@ app.PhotoView = Backbone.View.extend({
    //className: 'col-xs-7 col-md-3 col-sm-4 col-lg-3',
    className: 'card',
 
-template: _.template('<!--<h5></h5>--><div class="card-image"><a href=<%= picPath %> data-lightbox=<%= _id %> ><img data-src="holder.js/100%x180" alt=<%= author %> style="height: 180px; width: 100%; display:block;" src=<%= picPath %> </a><% if(title!=="") {%><h5><%= title%></h5><%}%></div><% if(message!=="") {%><p><%= message %></p><%}%>'),
+template: _.template('<!--<h5></h5>--><div class="card-image"><a href=<%= picPath %> data-lightbox=<%= _id %> > <img data-src="holder.js/100%x180" alt=<%= author %> style="height: 180px; width: 100%; display:block;" src=<%= picPath %> </a> <% if(title!=="") {%><h5><%= title%></h5><%}%> </div> <% if(message!=="") {%> <p><%= message %></p> <%}%> <p id="time-stamp"><%=year %>-<%= month %>-<%= day %></p>'),
 
    render: function(){
      //var html = '<h3>' + this.model.get('author') + '</h3>';
