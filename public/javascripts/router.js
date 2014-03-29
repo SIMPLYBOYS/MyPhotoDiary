04d5c3e1942cb2be8fe2113b9a73b63d4f5979ee
aaron_chou <ferrari828@gmail.com>
2014-03-29 15:21:27 +0000

    Change router object to AMD

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,111 +1,67 @@
-var app = app || {};
+var app = app || {}
 
-var PhotoDiaryApp = new (Backbone.Router.extend({
-  routes: {
-              '' : 'index',    
-           'demo': 'demo',
-           'about': 'about',
-           'diaries/:year/:month/:id': 'detail_diary_year',
-           'file/*path': 'file',
-           'contact': 'contact',
-           'others': 'other_demo',
-           'year/:y': 'year_diary',
-           'cardui_3': 'cardui_demo',
-           'masonry': 'masonry'
-           //'*path': 'notFound'
-  },
-  file: function(path){
-    alert('file');
-    var parts = path.split('/');
-    console.log(parts);
-  },
-  notFound: function(path){
-    alert('Sorry! There is no content here!.');
-  },
-  initialize: function(){
-    this.DemoDiary = new app.Diary(app.Photo); 
-    this.MasonryDiary = new app.MasonryDiary(app.Photo);
-  }, 
-  start: function(){
-    //alert('history.start!');
-    Backbone.history.start({pushState: true});
-  },
-  index: function(){
-    $('#diary_num').hide();
-    //alert("index router !!" + this.DemoDiary.length);
-    /*this.Diary.fetch();
-    this.DiaryView = new app.DiaryView({collection: this.Diary});
-    $('.row.demo').append(this.DiaryView.el);*/
-  },
-  about: function(){
-    $('#diary_num').hide();
-  },
-  demo: function(){
-    //alert('demo!');
-    var  DemoDiaryListView = new app.DiaryListView({collection: this.DemoDiary});
-    this.DemoDiary.fetch({reset: true}); 
-    /*this.DemoDiary.fetch();
-    this.DemoDiary.trigger('reset');*/
-    //alert(this.DemoDiary.authorCount());
-    //$('.row.demo').append(DemoDiaryListView.el);   
-  },
-  masonry: function(){
-    $('#diary_num').hide();
-    var DiaryMansonryView = new app.DiaryMasonryView({collection: this.MasonryDiary});
-  },
-  cardui_demo: function(){
-    $('#diary_num').hide();
-    var DemoDiaryCardView = new app.DiaryCardView({collection: this.MasonryDiary});
-    //var DemoDiaryCardView = new app.DiaryCardView({collection: this.MasonryDiary}); 
-    //$('.container').append(DemoDiaryCardView.el);
-    //$('row.demo').append(DemoDiaryCardView.el);
-    //alert('width of the window: '+$(document).width());
-  },
-  contact: function(){
-    $('#diary_num').hide();
-    var ContatDiaryListView = new app.DiaryWallView({collection: this.DemoDiary});
-    this.DemoDiary.fetch({reset: true});
-    $('.row.demo').append(ContactDiaryListView.el);
-    //$('.photowall').photoWall({speed: 500});
-  },
-  other_demo: function(){
-    alert('other demos!');
-    /*var  DemoDiaryView = new app.DiaryView({collection: this.DemoDiary});
-    this.DemoDiary.fetch({reset: true});
-    $('.row.aaron').append(DemoDiaryView.el);*/
-  },
-  year_diary: function(y){
-    switch (y) {
-      case '2014':
-        var yearDiaryView = new app.DiaryYearListView({collection: this.DemoDiary},{year: '2014'});
-        this.DemoDiary.fetch({reset: true});
-        break;
-      case '2013':
-        var yearDiaryView = new app.DiaryYearListView({collection: this.DemoDiary},{year: '2013'});
-        this.DemoDiary.fetch({reset: true});
-        break;
-      //alert('2014 shown '+ y + this.DemoDiary.yearShow2014());
-      default:
-        console.log('not in DB!');
-        break;
-    }
-  },
-  detail_diary_year: function(year, month, id){
-    console.log('month: ' + month);
-    switch (year){
-      case '2014':
-        //alert('router works  !!' + year + year + month + id);
-        var diaryDetailView = new app.DiaryDetailView({collection: this.DemoDiary},{year: '2014'}, {month: month});
-        this.DemoDiary.fetch({reset: true});
-        console.log(id);
-        break;
-      case '2013':
-        var diaryDetailView = new app.DiaryDetailView({collection: this.DemoDiary},{year: '2013'}, {month: month});
-        this.DemoDiary.fetch({reset: true});
-        break;
-      default:
-        alert('not in DB!');
-        break; 
-    }
-  }
-}));
+define(['jquery',
+	'underscore',
+	'backbone',
+        'models/photo',
+        'collections/diary',
+        'collections/masonry_diary',
+        'views/diarycardview',
+        'masonry',
+        'infiniscroll',
+        'bootstrap'
+],function($, _, Backbone){
+
+	var AppRouter = Backbone.Router.extend({
+            routes: {
+              '': 'index',
+             'demo': 'demo',
+             'about': 'about',
+             'diaries/:year/:month/:id': 'detail_diary_year',
+             'diary_per_day/uploads/:path': 'diary_per_day',
+             'file/*path': 'file',
+             'contact': 'contact',
+             'others': 'other_demo',
+             'year/:y': 'year_diary',
+             'cardui_3': 'cardui_3',
+             'masonry': 'masonry'
+           },
+           index: function(){
+	     console.log('--- index ---');
+	     $('#diary_num').hide();
+           },
+           start: function(){
+	     console.log('--- start ---');
+	     Backbone.history.start({pushSate: true});
+           },
+           initialize: function(){
+              console.log('--- initialiize ---');
+              this.DemoDiary = new app.Diary(app.Photo);
+              this.MasonryDiary = new app.MasonryDiary(app.Photo);
+              this.route('/cardui_3', "cardui_3");
+           },
+           about: function(){
+              console.log('--- about ---');
+              $('#diary_num').hide();
+           },
+           contact: function(){
+              $('#diary_num').hide();
+           },
+           cardui_3: function(){
+              //app.AppRouter.navigate('cardui_3', {trigger: true});
+              $('#diary_num').hide();
+              console.log('cardui_3');
+              var DemoDiaryCardView = new app.DiaryCardView({collection: this.MasonryDiary});
+           } 
+        });
+
+        var initialize = function(){
+	   app.AppRouter = new AppRouter();
+           app.AppRouter.start();
+           //app.AppRouter.navigate('cardui_3', {trigger: true});
+        };
+	
+	return{
+  		initialize: initialize
+        };
+});
