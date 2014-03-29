35298015b1522bdec3c93a4e9816e2338923cf5e
aaron_chou <ferrari828@gmail.com>
2014-03-29 15:59:28 +0000

    Update diary collection object to AMD

--- public/javascripts/collections/diary.js
+++ public/javascripts/collections/diary.js
@@ -1,19 +1,56 @@
 var app = app || {};
 
-app.Diary = Backbone.Collection.extend({
+define(['underscore',
+         'backbone',
+         'models/photo'
+],function(_, Backbone){
+  app.Diary = Backbone.Collection.extend({
+    model: app.Photo,
+      url: '/api/diaries',
+    comparator: function(item1, item2){
+      return item1.get('year') < item2.get('year');
+    },
+
+    parse: function(res){
+      return res;
+    },
+
+    authorCount: function(){
+      return this.where({author: 'aaron'}).length;
+    },
+
+    dateCount: function(){
+      return this.where({year: '2014'}).length;
+    },
+
+    yearShow2014: function(m){
+      if(m!='all'){
+       console.log('yearShow2014 -----' + m + '----\n');
+       return this.where({year: '2014'});
+      } else {
+        console.log('all year list');
+        return this.where({year: '2014'});
+      }
+    },
+
+    yearShow2013: function(){
+      return this.where({year: '2013'});
+    },
+
+    showLatest: function(){
+     return this.findWhere({year: '2014'});
+    }
+  });
+});
+
+/*app.Diary = Backbone.Collection.extend({
   model: app.Photo,
     url: '/api/diaries',
-    //url: '/year/2014',
-  //comparator: 'year',
   comparator: function(item1, item2){
     return item1.get('year') < item2.get('year');
   },
   
   parse: function(res){
-    //alert(JSON.stringify(res));
-    //return this.where({year: '2014'});
-    /*var sort = _.where(res, {year: '2014'});
-    return sort;*/
     return res;
   },
 
@@ -28,8 +65,6 @@ app.Diary = Backbone.Collection.extend({
   yearShow2014: function(m){
     if(m!='all'){
      console.log('yearShow2014 -----' + m + '----\n');
-     //return this.where({year: '2014', month: m});
-     //this.sort({year: '2014'});
      return this.where({year: '2014'});
     } else {
       console.log('all year list');
@@ -44,4 +79,4 @@ app.Diary = Backbone.Collection.extend({
   showLatest: function(){
    return this.findWhere({year: '2014'});
   }
-});
+});*/
