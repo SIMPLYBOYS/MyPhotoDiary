7e4caf881185aeff91f56175dadd219147de6c26
aaron_chou <ferrari828@gmail.com>
2014-03-29 16:01:40 +0000

    Update diary_masonry view cbject to AMD

--- public/javascripts/views/diarymasonryview.js
+++ public/javascripts/views/diarymasonryview.js
@@ -1,19 +1,18 @@
 var app = app || {},
     elems = [],
     notifElem,
+    index= 0,
     notifyTimeout;
 
-
-app.DiaryMasonryView = Backbone.View.extend({
+define([
+  'underscore',
+  'backbone', 
+],function(_, Backbone){
+  app.DiaryMasonryView = Backbone.View.extend({
   el: '#container',
 
-  events:{
-    //TODO
-  },
-
   initialize: function(){
-     //this.collection = new app.Diary();
-    //this.collection.on('reset', this.render, this); 
+
     _.bindAll(this, 'addAll', 'addOne', 'appendOne', 'appendView','checkEnd');
     notifElem = $('#notification');
 
@@ -25,17 +24,14 @@ app.DiaryMasonryView = Backbone.View.extend({
     $.scrollUp({
       scrollDistance:300,
       scrollSpeed: 50,
-      animation: 'fade',  
+      animation: 'fade',
       animationInSpeed: 200,
       animationOutSpeed: 200,
-      scrollText: 'Back to Top' 
-    }); 
-    /*content = $('.container');
-    this.layoutColumns();
-    $(window).resize(this.onResize);
-    //this.collection.fetch({reset: true});*/
-    
-  }, 
+      scrollText: 'Back to Top'
+    });
+
+  },
+
   notify: function(message){
     notifElem.html(message);
     notifElem.css("transition","none");
@@ -45,129 +41,96 @@ app.DiaryMasonryView = Backbone.View.extend({
     if(notifyTimeout){
       clearTimeout(notifyTimeout);
     }
- 
+
     notifyTimeout = setTimeout(this.hideNotify, 1000);
   },
+ 
   hideNotify: function(){
     notifElem.css("transition","opacity 1.0s");
     notifElem.css("opacity","0");
   },
+
   addOne: function(photo){
-    console.log('addOne\n');
-    var photomasonryView = new app.PhotoMasonryView({model: photo});
-    this.$el.append(photomasonryView.render().el);
+    index++;
+    console.log('addOne with index:' + index + '\n');
+    if(index > 2){
+      var photomasonryView = new app.PhotoMasonryView({model: photo});
+      this.$el.append(photomasonryView.render().el);
+    } else if(index === 1) {
+      var stampView = new app.StampView({model: photo});
+      this.$el.append(stampView.render().el);
+    } else if(index ===2) {
+      var topicView = new app.TopicView({model: photo});
+      this.$el.append(topicView.render().el);
+    }
   },
+
   addAll: function(){
 
-     //alert('length of collection: '+ this.collection.length);
-
-    /*content = $('.container');
-    this.layoutColumns();
-    $(window).resize(this.onResize);*/
-    //alert('$(document).width:' + $(document).width());
-
-    //alert('DiaryListView render!' + this.collection.yearShow2014().length);
-
-    /*$('.badge').html(this.collection.yearShow2014().length + this.collection.yearShow2013().length);*/
     this.$el.empty();
     this.collection.forEach(this.addOne, this);
+    $('.group1').colorbox({rel:'group1', speed:500, width:'80%', height:'80%'});
     var $container = $('#container').masonry();
-    
-    //----------------------------------------------------
-    /*$container.imagesLoaded(function(){
-       $container.masonry({
-         itemSelector: '.picbox',
-         columnWidth: 300
-       });
-    });
-
-    $container.infinitescroll({
-      navSelector: '#page-nav',
-      nextSelector: '#page-nav a',
-      itemSelector: '.picbox',
-      loading: {
-        finishedMsg: 'No more pages to load.',
-        img: 'http://i.imgur.com/6RMhx.gif'
-       }
-      },
-      function(newElements){
-       alert('Testing!');
-        var $newElems = $(newElements);
-        $newElems.imagesLoaded(function(){
-         $container.masonry('appended', $newElems, true);
-        });
-      }
-    );*/
-
-    // -----------------------------------------------------
-    
-    
-    
     $container.masonry('bindResize');
     $('#container').css({ opacity: 0 });
     $container.imagesLoaded().done(function(instance){
       $('#container').animate({ opacity:1 });
       $container.masonry({
-          itemSelect: '.picbox .demobox',
-          columnWidth: 300,
+          itemSelect: '.picbox .stamp .card .topic',
+          columnWidth: 10,
           isAnimated: true,
           isFitWidth: true,
           animate: true
         });
     }).progress(function( instance, image){
       var result = image.isLoaded ? 'loaded' : 'broken';
-      //console.log('images is ' + result + ' for ' + image.img.src);
     });
-
-    /*setInterval(function(){
-      alert('test setTimeout');
-    },500);*/
-
-
-    // --------------------------------------------- work around 
-    /*setTimeout(function(){
-      var $container = $('#container').masonry();
-      $container.imagesLoaded(function(){
-         $container.masonry({
-          itemSelect: '.picbox .demobox',
-          columnWidth: 300,
-          isAnimated: true,
-          animate: true
-        });
-      }); 
-    },500);*/
   },
+
   checkEnd: function(){
     this.notify('Reach the End of MasonryPage!');
   },
+
   appendView: function(collection, response){
-   //alert(JSON.stringify(response[0]));
-   //_.each(response, this.addOne, this);
-   //response.forEach(this.addOne, this);
    var $container = $('#container').masonry({itemSelect: '.picbox .demobox',
-          columnWidth: 300,
+          columnWidth: 10,
           isAnimated: true,
           isFitWidth: true,
-          animate: true  
+          animate: true
+        });
+    }).progress(function( instance, image){
+      var result = image.isLoaded ? 'loaded' : 'broken';
+    });
+  },
+
+  checkEnd: function(){
+    this.notify('Reach the End of MasonryPage!');
+  },
+
+  appendView: function(collection, response){
+   var $container = $('#container').masonry({itemSelect: '.picbox .demobox',
+          columnWidth: 10,
+          isAnimated: true,
+          isFitWidth: true,
+          animate: true
         }),
         appendlist;
 
-   $container.masonry('on', 'layoutComplete', function(msnyInstance, laidOutItems){ 
-    //console.log('length of laidOutItems: ' + laidOutItems.length); 
-    //msnyInstance.hide();
+   $container.masonry('on', 'layoutComplete', function(msnyInstance, laidOutItems){
+    console.log('length of laidOutItems: ' + laidOutItems.length);
    })
 
    appendlist = _.map(response, function(listconfig){
-     //console.log(listconfig);
-     return new app.Photo(listconfig); 
+     console.log(listconfig);
+     return new app.Photo(listconfig);
    });
-   //alert(JSON.stringify(response));
+
    appendlist.forEach(this.appendOne, this);
-   //$(elems).animate({ opacity:0 });  
+   $('.group1').colorbox({rel:'group1', speed:500, width:'80%', height:'80%'});
    $('footer p').animate({ opacity:1 });
-   
-   $container.append(elems).masonry('appended', elems); 
-   elems.splice(0,elems.length); 
+
+   $container.append(elems).masonry('appended', elems);
+   elems.splice(0,elems.length);
    if(response.length > 0){
      this.notify('Have Next Page!');
    } else {
@@ -175,8 +138,11 @@ app.DiaryMasonryView = Backbone.View.extend({
    }
    console.log('response.length: ' + response.length + '\ncollection.length: ' + collection.length + '\nelems: ' + elems.length);
   },
+
   appendOne: function(photo){
     var photomasonryView = new app.PhotoMasonryView({model: photo});
     elems.push(photomasonryView.render().el);
   }
 });
+});
+
